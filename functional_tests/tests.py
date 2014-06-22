a2dc47f6c7c58a6de86dcfc3f88f7713b9e02c4b
frank0000 <frank_oh@hotmail.com>
2014-06-21 17:41:15 -0700

    hacked FT runner to be able to test staging also

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,27 @@
+import sys
+import unittest
+import time
+
 from django.contrib.staticfiles.testing import StaticLiveServerCase
 
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 
-import unittest
-import time
-
 class NewVisitorTest(StaticLiveServerCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox() 
@@ -24,7 +39,7 @@ class NewVisitorTest(StaticLiveServerCase):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # verifies the page has something to do with To-Do
         self.assertIn('To-Do', self.browser.title)
@@ -65,7 +80,7 @@ class NewVisitorTest(StaticLiveServerCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -96,7 +111,7 @@ class NewVisitorTest(StaticLiveServerCase):
 
     def test_layout_and_styling(self):
         # Edit goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
