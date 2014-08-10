069a4c0978316227b73278fa8982f052170850ba
frank0000 <frank_oh@hotmail.com>
2014-08-09 22:48:36 -0700

    placeholder test_my_lists FT and move login checkers into base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,6 +1,8 @@
-import sys
 from django.contrib.staticfiles.testing import StaticLiveServerCase
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+import sys
+
 
 class FunctionalTest(StaticLiveServerCase):
 
@@ -18,17 +20,41 @@ class FunctionalTest(StaticLiveServerCase):
         if cls.server_url == cls.live_server_url:
             super().tearDownClass()
 
+
     def setUp(self):
-        self.browser = webdriver.Firefox() 
+        self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
 
     def tearDown(self):
         self.browser.quit()
-        
+
+
     def get_item_input_box(self):
         return self.browser.find_element_by_id('id_text')
+
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
+
+
+    def wait_for_element_with_id(self, element_id):
+        WebDriverWait(self.browser, timeout=30).until(
+            lambda b: b.find_element_by_id(element_id),
+            'Could not find element with id {}. Page text was {}'.format(
+                element_id, self.browser.find_element_by_tag_name('body').text
+            )
+        )
+
+
+    def wait_to_be_logged_in(self, email):
+        self.wait_for_element_with_id('id_logout')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertIn(email, navbar.text)
+
+
+    def wait_to_be_logged_out(self, email):
+        self.wait_for_element_with_id('id_login')
+        navbar = self.browser.find_element_by_css_selector('.navbar')
+        self.assertNotIn(email, navbar.text)
