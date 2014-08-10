e22b7cefef41a55dd3892dbf5cd2dbcfd9de767b
frank0000 <frank_oh@hotmail.com>
2014-08-09 21:41:47 -0700

    Custom persona auth backend + custom user model, working login FT

--- accounts/static/accounts.js
+++ accounts/static/accounts.js
@@ -1,23 +1,22 @@
-var initialize = function(navigator, user, token, urls) {
-    $('#id_login').on('click', function() {
+/*global $ */
+
+var initialize = function (navigator, user, token, urls) {
+    $('#id_login').on('click', function () {
         navigator.id.request();
     });
+
     navigator.id.watch({
         loggedInUser: user,
-        onlogin: function(assertion) {
-            $.post(urls.login, 
-                {assertion: assertion,
-                 csrfmiddlewaretoken: token}
+        onlogin: function (assertion) {
+            console.log('onlogin')
+            $.post(
+                urls.login,
+                { assertion: assertion, csrfmiddlewaretoken: token }
             )
-            .done(function() {
-               window.location.reload();
-            })
-            .fail(function() {
-                navigator.id.logout();
-            });
+                .done(function () { console.log('DONE'); window.location.reload(); })
+                .fail(function () { console.log('FAIL'); navigator.id.logout(); });
         },
-        onlogout: function() {
-        }
+        onlogout: function () {}
     });
 };
 
